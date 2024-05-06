6a8c8eef51e18d50c59b13874d7e2bb65b9fcc5c
aurearbor <arthur.goldenbaum@gmail.com>
2024-05-06 11:07:01 +0200

    setup complete

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -2,7 +2,6 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
-import '../node_modules/bootstrap/dist/css/bootstrap.min.css';
 
 import { applyMiddleware, legacy_createStore as createStore } from 'redux';
 import { Provider } from 'react-redux';
