f75c1a71ac2e792ddac642b43b82155d2037ea06
jala5026 <jala5026@bht-berlin.de>
2024-05-09 13:38:36 +0200

    better error handling --UNTESTED

--- install.sh
+++ install.sh
@@ -1,13 +1,20 @@
 !#/bin/bash
 
-
+echo "Installing npm packages"
 #install npm packages
 npm install
+#check if the install went throuth
+if [ $? -ne 0 ]; then
+    printf '%s\n' "Something went wrong. Please make sure npm is installed and setup correctly. npm is a prerequisit." >&2
+    exit 1
+fi
 
 #Start frontend
+echo "Starting frontend"
 cd frontend
 &npm start
 
 #Start backend
+echo "Starting backend"
 cd ../backend
 &npm start
--- test.sh
+++ test.sh
@@ -1,13 +1,21 @@
 !#/bin/bash
 
 
+echo "Installing npm packages"
 #install npm packages
 npm install
+#check if the install went throuth, if not quit with error
+if [ $? -ne 0 ]; then
+    printf '%s\n' "Something went wrong. Please make sure npm is installed and setup correctly. npm is a prerequisit." >&2
+    exit 1
+fi
 
 #Start frontend
+echo "running frontend tests"
 cd frontend
 npm test
 
 #Start backend
+echo "running backend tests"
 cd ../backend
 npm test
