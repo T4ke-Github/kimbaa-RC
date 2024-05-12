86533c213f2bd4eda14a02f41d26d8d7fe6a25c9
SaschaAthmer <33932092+J0hnExample@users.noreply.github.com>
2024-05-12 17:41:30 +0200

    working login post

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -2,17 +2,18 @@ import express from 'express';
 import "express-async-errors"; // needs to be imported before routers and other stuff!
 
 import { userRouter } from '../src/routes/user';
+import { loginRouter } from '../src/routes/login';
+import bodyParser from 'body-parser';
 
 
 const app = express();
 
 // Middleware:
 app.use('*', express.json()) //
+app.use(bodyParser.json())
 
 // Routes
-//app.use("/api/login", loginRouter)   // wird erst später implementiert, hier nur Dummy; hat aber bereits einen Präfix
-
-// TODO: Registrieren weiterer Router:
+app.use("/api/login", loginRouter)   
 app.use("/api/user", userRouter)
 
 export default app;
---
+++ backend/src/routes/login.ts
@@ -0,0 +1,26 @@
+import express from "express";
+import * as userService from "../services/UserService";
+import * as authenticationService from "../services/AuthenticationService";
+
+export const loginRouter = express.Router();
+
+// Route für den Login
+loginRouter.post("/login", async (req, res, next) => {
+    try {
+        const { studentId, password } = req.body; // Annahme: Student-ID und Passwort werden im Anfragekörper übergeben
+
+        // Überprüfe die Anmeldeinformationen
+        const loginResult = await authenticationService.login(studentId, password);
+        
+        if (loginResult) {
+            // Erfolgreicher Login
+            res.status(200).json(loginResult); // Gib Benutzer-ID und Rolle zurück
+        } else {
+            // Fehlgeschlagener Login
+            res.status(401).send("Unauthorized"); // Oder eine angemessene Fehlermeldung
+        }
+    } catch (error) {
+        res.status(500).send("Internal Server Error");
+        next(error);
+    }
+});
