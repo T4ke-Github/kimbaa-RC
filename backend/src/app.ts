e0d11604cd0d0ffb0174c42e93ad5d2577510e22
SaschaAthmer <33932092+J0hnExample@users.noreply.github.com>
2024-05-16 10:16:32 +0200

    fixes

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -12,7 +12,7 @@ const app = express();
 
 // Middleware:
 app.use(cors({
-    origin: 'http://localhost:3000' // Replace with your frontend URL
+    origin: 'http://localhost:8080' // Replace with your frontend URL
 }));
 app.use('*', express.json()) //
 app.use(bodyParser.json())
