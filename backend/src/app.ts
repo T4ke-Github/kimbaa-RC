6ad795bad09d175c05daf0d4310ecbbff2785a41
SaschaAthmer <33932092+J0hnExample@users.noreply.github.com>
2024-05-29 22:20:12 +0200

    removed user to console in loggin

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -17,7 +17,7 @@ const app = express();
 
 app.use(cors({
     origin: function (origin, callback) {
-        const allowedOrigins = ['http://127.0.0.1:3000', 'http://localhost:3000'];
+        const allowedOrigins = ['127.0.0.1:3000', 'localhost:3000'];
         if (!origin || allowedOrigins.indexOf(origin) !== -1) {
             callback(null, true)
         } else {
